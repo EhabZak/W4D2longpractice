7c9d86ad6c51fc85eb3df88e72653bb86f2a1c0c
Rebecca <mendez_elizabeth88@hotmail.com>
2023-05-09 15:48:05 -0700

    friendly dragon complete, evil dragon partial

--- all-dragons.js
+++ all-dragons.js
@@ -1,6 +1,14 @@
-// Your code here
+const FriendlyDragon = require('./classes/friendly-dragon');
+const EvilDragon = require('./classes/evil-dragon')
 
+const falkor = new FriendlyDragon ('Falkor', 'white',
+['save Atreyu from the swamp', 'save Atreyu from the Nothing', 'scare the local bullies into a dumpster'],
+'Bastian')
 
+console.log(falkor);
+falkor.hasLifeGoals();
+console.log(falkor.breathesFire());
+console.log(falkor.helpsPeople());
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
